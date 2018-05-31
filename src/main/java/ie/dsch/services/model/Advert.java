04e3b4c1c1f110901df2e40d5b6c0fbc5357b286
Plínio Cardoso <pliniocardoso@Plinios-MacBook-Pro.local>
2018-05-31 15:04:06 +0100

    Added not null message

--- src/main/java/ie/dsch/services/model/Advert.java
+++ src/main/java/ie/dsch/services/model/Advert.java
@@ -16,14 +16,14 @@ public class Advert {
     @Id
     private String id;
 
-    @NotNull
+    @NotNull(message = "Title must be present")
     @Size(min = 10, max = 100, message = "Size of title is invalid (10 - 100)")
     private String title;
 
-    @NotNull
+    @NotNull(message = "Description must be present")
     @Size(min = 100, max = 500, message = "Size of description is invalid (100 - 500)")
     private String description;
 
-    @NotNull
+    @NotNull(message = "Price must be present")
     private Double price;
 }
